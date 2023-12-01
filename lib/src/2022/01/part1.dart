0e09f6847aae590033cd005a525c958ceeefb6c3
Franco Prudhomme <francoprud@gmail.com>
2023-12-01 23:29:23 +0100

    [2022-01] part 1

---
+++ lib/src/2022/01/part1.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'dart:io';
+
+void main() {
+  final File file = File('lib/src/2022/01/input.txt');
+
+  int caloriesMax = 0, caloriesCount = 0;
+
+  file.openRead().transform(utf8.decoder).transform(LineSplitter()).listen(
+    (event) {
+      if (event.isEmpty) {
+        caloriesCount = 0;
+      } else {
+        final int calories = int.parse(event);
+
+        caloriesCount += calories;
+
+        if (caloriesMax < caloriesCount) {
+          caloriesMax = caloriesCount;
+        }
+      }
+    },
+    onDone: () => print(caloriesMax),
+  );
+}
