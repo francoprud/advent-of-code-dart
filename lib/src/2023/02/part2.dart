276f62ac9caadb2583e8229e41731891d7919e9d
Franco Prudhomme <francoprud@gmail.com>
2023-12-02 10:50:35 +0100

    [2023-02] part 2

---
+++ lib/src/2023/02/part2.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  final File file = File('lib/src/2023/02/input.txt');
+  int powerSum = 0;
+
+  file.openRead().transform(utf8.decoder).transform(LineSplitter()).listen(
+    (event) {
+      final List<int> gameConfiguration = getGameConfiguration(event);
+
+      powerSum += getColorsPowerSum(gameConfiguration);
+    },
+    onDone: () => print(powerSum),
+  );
+}
+
+// returns the configurations with the following format: [id, red, green, blue]
+List<int> getGameConfiguration(String s) {
+  return [
+    getIdFromGameRecord(s),
+    getColorFromGameRecord(s, 'red'),
+    getColorFromGameRecord(s, 'green'),
+    getColorFromGameRecord(s, 'blue')
+  ];
+}
+
+int getIdFromGameRecord(String s) {
+  RegExp exp = RegExp(r'Game (\d*)');
+
+  return int.parse(exp.firstMatch(s)!.group(1)!);
+}
+
+int getColorFromGameRecord(String s, String color) {
+  RegExp exp = RegExp('(\\d*) $color');
+
+  return exp.allMatches(s).map((match) => int.parse(match[1]!)).reduce(max);
+}
+
+int getColorsPowerSum(List<int> gameConfiguration) {
+  return gameConfiguration
+      .getRange(1, 4)
+      .reduce((value, element) => value * element);
+}
