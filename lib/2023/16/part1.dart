ae6eb6a629ffff504c6b34f9e24a1a4be559fd9c
Franco Prudhomme <francoprud@gmail.com>
2023-12-22 00:08:04 +0100

    [2023-16] part 1

---
+++ lib/2023/16/part1.dart
@@ -0,0 +1,63 @@
+import 'dart:collection';
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  List<List<String>> layout = getLayout('lib/2023/16/input.txt');
+  Queue<Beam> beamsQueue = Queue.of([Beam(Point(0, -1), Point(0, 1))]);
+  Set<Beam> seen = {};
+
+  while (beamsQueue.isNotEmpty) {
+    Beam previousBeam = beamsQueue.removeFirst();
+    Beam currentBeam = Beam(previousBeam.position + previousBeam.direction,
+        previousBeam.direction); // move the beam
+
+    if (seen.contains(currentBeam) || isOffRange(currentBeam.position, layout)) {
+      continue;
+    }
+
+    String c = layout[currentBeam.position.x][currentBeam.position.y];
+
+    if (c == '.' ||
+        (c == '-' && currentBeam.direction.y != 0) ||
+        (c == '|' && currentBeam.direction.x != 0)) {
+      beamsQueue.add(currentBeam);
+    } else if (c == '/') {
+      beamsQueue.add(Beam(currentBeam.position,
+          Point(-currentBeam.direction.y, -currentBeam.direction.x)));
+    } else if (c == '\\') {
+      beamsQueue.add(Beam(currentBeam.position,
+          Point(currentBeam.direction.y, currentBeam.direction.x)));
+    } else if (c == '|') {
+      beamsQueue.add(Beam(currentBeam.position, Point(1, 0)));
+      beamsQueue.add(Beam(currentBeam.position, Point(-1, 0)));
+    } else if (c == '-') {
+      beamsQueue.add(Beam(currentBeam.position, Point(0, 1)));
+      beamsQueue.add(Beam(currentBeam.position, Point(0, -1)));
+    }
+
+    seen.add(currentBeam);
+  }
+
+  print(Set.from(seen.map((beam) => beam.position)).length);
+}
+
+List<List<String>> getLayout(String filename) =>
+    File(filename).readAsLinesSync().map((line) => line.split('')).toList();
+
+bool isOffRange(Point p, List<List<String>> layout) =>
+    p.x < 0 || p.y < 0 || p.x >= layout.length || p.y >= layout[0].length;
+
+class Beam {
+  Point<int> position;
+  Point<int> direction; // the direction the beam is going
+
+  Beam(this.position, this.direction);
+
+  bool operator ==(Object other) =>
+      other is Beam && position == other.position && direction == other.direction;
+
+  int get hashCode => Object.hash(position, direction);
+
+  String toString() => 'Beam: $position | $direction';
+}
