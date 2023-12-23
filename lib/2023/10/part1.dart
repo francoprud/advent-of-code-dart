d38d7959b46203d67a03bd2204907a83cb4fa1bf
Franco Prudhomme <francoprud@gmail.com>
2023-12-23 14:19:10 +0100

    [2023-10] part 1 refactor

--- lib/2023/10/part1.dart
+++ lib/2023/10/part1.dart
@@ -3,71 +3,82 @@ import 'dart:io';
 import 'dart:math';
 
 void main() {
-  List<List<String>> board = getBoard('lib/2023/10/input.txt');
-  Point<int> startingPosition = getStartingPosition(board);
-  Queue<Point<int>> queue = Queue<Point<int>>.from([startingPosition]);
+  Maze maze = Maze('lib/2023/10/input.txt');
+  maze.findLoop();
+  print(maze.visited.length ~/ 2);
+}
+
+class Maze {
+  late List<List<String>> board;
+  late Point<int> startingPosition;
   Set<Point<int>> visited = {};
 
-  while (queue.isNotEmpty) {
-    Point<int> currentPosition = queue.removeFirst();
-    visited.add(currentPosition);
+  Maze(String filename) {
+    this.board = _getBoard(filename);
+    this.startingPosition = _getStartingPosition();
+  }
 
-    List<Point<int>> possibleMoves = getPossibleMoves(board, currentPosition);
-    for (Point<int> move in possibleMoves) {
-      if (!visited.contains(move)) {
-        queue.add(move);
+  List<List<String>> _getBoard(String filename) =>
+      File(filename).readAsLinesSync().map((line) => line.split('')).toList();
+
+  Point<int> _getStartingPosition() {
+    for (int row = 0; row < board.length; row++) {
+      for (int column = 0; column < board[row].length; column++) {
+        if (board[row][column] == 'S') return Point<int>(row, column);
+      }
+    }
+
+    throw Exception('No starting position found');
+  }
+
+  void findLoop() {
+    Queue<Point<int>> queue = Queue<Point<int>>.from([startingPosition]);
+
+    while (queue.isNotEmpty) {
+      Point<int> currentPosition = queue.removeFirst();
+      visited.add(currentPosition);
+
+      List<Point<int>> possibleMoves = getPossibleMoves(currentPosition);
+      for (Point<int> move in possibleMoves) {
+        if (!visited.contains(move)) {
+          queue.add(move);
+        }
       }
     }
   }
 
-  print(visited.length ~/ 2);
-}
+  List<Point<int>> getPossibleMoves(Point<int> position) {
+    List<Point<int>> possibleMoves = [];
+    String tile = board[position.x][position.y];
 
-List<List<String>> getBoard(String filename) {
-  return File(filename).readAsLinesSync().map((line) => line.split('')).toList();
-}
-
-Point<int> getStartingPosition(List<List<String>> board) {
-  for (int row = 0; row < board.length; row++) {
-    for (int column = 0; column < board[row].length; column++) {
-      if (board[row][column] == 'S') return Point<int>(row, column);
+    // going up: tile must be S, |, J or L, and the tile above must be |, 7 or F
+    if (position.x > 0 &&
+        '|SJL'.contains(tile) &&
+        '|7F'.contains(board[position.x - 1][position.y])) {
+      possibleMoves.add(Point<int>(position.x - 1, position.y));
     }
+
+    // going down: tile must be S, |, 7 or F, and the tile below must be |, J or L
+    if (position.x < board.length - 1 &&
+        '|S7F'.contains(tile) &&
+        '|JL'.contains(board[position.x + 1][position.y])) {
+      possibleMoves.add(Point<int>(position.x + 1, position.y));
+    }
+
+    // going left: tile must be S, -, 7 or J, and the tile to the left must be -, L or F
+    if (position.y > 0 &&
+        '-S7J'.contains(tile) &&
+        '-LF'.contains(board[position.x][position.y - 1])) {
+      possibleMoves.add(Point<int>(position.x, position.y - 1));
+    }
+
+    // going right: tile must be S, -, F or L, and the tile to the right must be -, J or 7
+    if (position.y < board[position.x].length - 1 &&
+        '-SFL'.contains(tile) &&
+        '-J7'.contains(board[position.x][position.y + 1])) {
+      possibleMoves.add(Point<int>(position.x, position.y + 1));
+    }
+
+    return possibleMoves;
   }
-
-  throw Exception('No starting position found');
-}
-
-List<Point<int>> getPossibleMoves(List<List<String>> board, Point<int> position) {
-  List<Point<int>> possibleMoves = [];
-  String tile = board[position.x][position.y];
-
-  // going up: tile must be S, |, J or L, and the tile above must be |, 7 or F
-  if (position.x > 0 &&
-      '|SJL'.contains(tile) &&
-      '|7F'.contains(board[position.x - 1][position.y])) {
-    possibleMoves.add(Point<int>(position.x - 1, position.y));
-  }
-
-  // going down: tile must be S, |, 7 or F, and the tile below must be |, J or L
-  if (position.x < board.length - 1 &&
-      '|S7F'.contains(tile) &&
-      '|JL'.contains(board[position.x + 1][position.y])) {
-    possibleMoves.add(Point<int>(position.x + 1, position.y));
-  }
-
-  // going left: tile must be S, -, 7 or J, and the tile to the left must be -, L or F
-  if (position.y > 0 &&
-      '-S7J'.contains(tile) &&
-      '-LF'.contains(board[position.x][position.y - 1])) {
-    possibleMoves.add(Point<int>(position.x, position.y - 1));
-  }
-
-  // going right: tile must be S, -, F or L, and the tile to the right must be -, J or 7
-  if (position.y < board[position.x].length - 1 &&
-      '-SFL'.contains(tile) &&
-      '-J7'.contains(board[position.x][position.y + 1])) {
-    possibleMoves.add(Point<int>(position.x, position.y + 1));
-  }
-
-  return possibleMoves;
 }
