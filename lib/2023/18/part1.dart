16f573c734fb8b756218d1dde861e1e095e277b2
Franco Prudhomme <francoprud@gmail.com>
2024-01-01 12:15:39 +0100

    [2023-18] part 1

---
+++ lib/2023/18/part1.dart
@@ -0,0 +1,59 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  DigPlan digPlan = DigPlan();
+
+  File('lib/2023/18/input.txt')
+      .openRead()
+      .transform(utf8.decoder)
+      .transform(LineSplitter())
+      .listen(
+        (event) => digPlan.parseAction(event),
+        onDone: () => print(digPlan.calculateCubicMeters()),
+      );
+}
+
+class DigPlan {
+  Point<int> _currentCoordinate = Point(0, 0);
+  List<Point<int>> _coordinates = [Point(0, 0)];
+
+  void parseAction(String s) {
+    List<String> parts = s.split(' ');
+    String direction = parts[0];
+    int steps = int.parse(parts[1]);
+
+    for (int i = 0; i < steps; i++) {
+      if (direction == 'R') {
+        _currentCoordinate = Point(_currentCoordinate.x, _currentCoordinate.y + 1);
+      } else if (direction == 'L') {
+        _currentCoordinate = Point(_currentCoordinate.x, _currentCoordinate.y - 1);
+      } else if (direction == 'D') {
+        _currentCoordinate = Point(_currentCoordinate.x + 1, _currentCoordinate.y);
+      } else if (direction == 'U') {
+        _currentCoordinate = Point(_currentCoordinate.x - 1, _currentCoordinate.y);
+      }
+
+      Point<int> newCoordinate = Point(_currentCoordinate.x, _currentCoordinate.y);
+      _coordinates.add(newCoordinate);
+    }
+  }
+
+  // combination of Gauss's area formula (Shoelace) and Pick's theorem
+  int calculateCubicMeters() {
+    // Gauss's area formula (Shoelace)
+    int area = 0;
+    for (int i = 1; i < _coordinates.length - 1; i++) {
+      area += _coordinates[i].x * (_coordinates[i + 1].y - _coordinates[i - 1].y);
+    }
+    area = area.abs() ~/ 2;
+
+    // Pick's theorem
+    int boundaryPoints = _coordinates.length;
+    int interiorPoints = area - boundaryPoints ~/ 2 + 1;
+
+    // removing the double (0, 0) point
+    return interiorPoints + boundaryPoints - 1;
+  }
+}
